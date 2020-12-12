ca14c19e84677bb4a0537b2408825f8e2fe8ab14
astrosebas <sebascrrzgax@gmail.com>
2020-12-11 19:33:54 -0700

    modificando cositas saludos

--- Tarea3_RuidoCielo.py
+++ Tarea3_RuidoCielo.py
@@ -8,16 +8,16 @@ import numpy as np
 import sys
 from astropy.io import fits
 
-size_x = 840
-size_y = 840
+size_x = 1000
+size_y = 1000
 
 mean = 0.05
 sigma = 0.44
 
 sky = np.random.normal(mean,sigma,(size_x,size_y))
 
-hdu_sky = fits.PrimaryHDU()
-hdu_sky.data = sky 
-hdu_sky.writeto('ruido_cielo2.fits')
+hdu_sky = fits.PrimaryHDU() #Fits
+hdu_sky.data = sky          #Data del header
+hdu_sky.writeto('SkyNoise.fits')
 
-print("Tarea 17 git")
+print("Tarea 17 git con programa de la Tarea 3 (Ruido del cielo)")
